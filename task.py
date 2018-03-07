20d7ffd7dfa1b9db51c71285ef8426752bef3ef1
Bartlomiej  Michalski <4michalski@taurus.fis.agh.edu.pl>
2018-03-07 18:33:45 +0100

    task.py also runnable

--- task.py
+++ task.py
@@ -54,4 +54,17 @@ class Matrix:
 				for k in range(len(B)):
 					C[i][j] += A[i][k]*B[k][j]
 		return Matrix(*unzip(C))
-		
+
+if __name__ == "__main__":
+	matrix_1 = Matrix(4,5,6,7)
+	matrix_2 = Matrix(2,8,1,5)
+
+	sumOfMatrices = matrix_1.sum(matrix_2)
+	productOfMatrices = matrix_1.product(matrix_2)
+
+	print( "Matrix 1: {}".format(matrix_1) )
+	print( "Matrix 2: {}".format(matrix_2) )
+
+
+	print( "Sum: {}".format(sumOfMatrices) )
+	print( "Product: {}".format(productOfMatrices) )
